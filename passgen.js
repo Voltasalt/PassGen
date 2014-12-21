1199e7153011570c5c0d3b9e651caec3ef1c29fd
Voltasalt <voltasalt@gmail.com>
2014-12-21 20:18:15 +0100

    Initial commit

---
+++ passgen.js
@@ -0,0 +1,33 @@
+$(function() {
+    function calc() {
+        var site = $("#siteName").val();
+        var pass = $("#masterPass").val();
+
+        var siteHash = site, passHash = pass;
+
+        for (var i = 0; i < 2000; i++) {
+            siteHash = sjcl.hash.sha256.hash(siteHash);
+            passHash = sjcl.hash.sha256.hash(passHash);
+        }
+
+        var out = JSON.parse(sjcl.encrypt(passHash, siteHash));
+
+        $("#output").val(out.ct);
+    }
+
+    $("#masterPass").keyup(function() {
+        calc();
+    });
+
+    $("#siteName").keyup(function() {
+        calc();
+    });
+
+    $("#show").click(function() {
+        if ($("#show").is(":checked")) {
+            $("#output").attr("type", "text");
+        } else {
+            $("#output").attr("type", "password");
+        }
+    });
+});
